c21c5159d563a62515f688f4f9139ed2a9483bf6
lana2-2karla <allanakarla1702@gmail.com>
2022-11-15 14:55:50 -0300

    :recycle: refactor: modified index

--- ng-finance-api/src/index.ts
+++ ng-finance-api/src/index.ts
@@ -1,18 +1,18 @@
 import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import { Users } from "./entity/User"
 
 AppDataSource.initialize().then(async () => {
 
     console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
+    const user = new Users()
+    user.username = "Timber"
+    user.password = "hashdasenhamuitosecreta"
+    user.accountId = 1
     await AppDataSource.manager.save(user)
     console.log("Saved a new user with id: " + user.id)
 
     console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
+    const users = await AppDataSource.manager.find(Users)
     console.log("Loaded users: ", users)
 
     console.log("Here you can setup and run express / fastify / any other framework.")
