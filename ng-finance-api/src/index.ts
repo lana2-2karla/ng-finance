2e29f2632a3a9d92e395478187da484191caf798
lana2-2karla <allanakarla1702@gmail.com>
2022-11-15 18:41:10 -0300

    :recycle: refactor: start express

--- ng-finance-api/src/index.ts
+++ ng-finance-api/src/index.ts
@@ -1,20 +1,12 @@
-import { AppDataSource } from "./data-source"
-import { Users } from "./entity/User"
+import express = require("express");
+import { AppDataSource } from "./data-source";
 
 AppDataSource.initialize().then(async () => {
+    const app = express()
 
-    console.log("Inserting a new user into the database...")
-    const user = new Users()
-    user.username = "Timber"
-    user.password = "hashdasenhamuitosecreta"
-    user.accountId = 1
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+	app.use(express.json())
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(Users)
-    console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
+	return app.listen(`rodando na porta ${process.env.PORT}`)
+    
 
 }).catch(error => console.log(error))
