f82e221414e17f87d805647f3aca74be0e1269e8
lana2-2karla <allanakarla1702@gmail.com>
2022-11-15 17:28:45 -0300

    :recycle: refactor: add environment variables

--- ng-finance-api/src/data-source.ts
+++ ng-finance-api/src/data-source.ts
@@ -1,14 +1,17 @@
+import 'dotenv/config'
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { Users } from "./entity/User"
 
+const port = process.env.DB_PORT as unknown as number | undefined
+
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "ngFinances",
+    host: process.env.DB_HOST,
+    port: port,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
     entities: [Users],
