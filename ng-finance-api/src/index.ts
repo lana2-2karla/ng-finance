4110df04ccf890c9f7ac14f3bb777cfda4aee23b
lana2-2karla <allanakarla1702@gmail.com>
2022-11-16 10:26:09 -0300

    :bug: fix: app.listen

--- ng-finance-api/src/index.ts
+++ ng-finance-api/src/index.ts
@@ -1,12 +1,13 @@
 import express = require("express");
 import { AppDataSource } from "./data-source";
 
+
 AppDataSource.initialize().then(async () => {
     const app = express()
 
 	app.use(express.json())
 
-	return app.listen(`rodando na porta ${process.env.PORT}`)
-    
+	app.listen(process.env.PORT, () => console.log(`ouvindo na porta ${process.env.PORT}`));
+
 
 }).catch(error => console.log(error))
