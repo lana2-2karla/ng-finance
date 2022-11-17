ba6f7dba8648a4d57f307d588c2685a0bbcb93ee
lana2-2karla <allanakarla1702@gmail.com>
2022-11-17 16:10:37 -0300

    :sparkles: feat: create class for custom errors

---
+++ ng-finance-api/src/helpers/api-error.ts
@@ -0,0 +1,10 @@
+export class ApiError extends Error {
+    // readonly - atributo somente para leitura
+	public readonly statusCode: number
+
+	constructor(message: string, statusCode: number) {
+        //super é para acessar a classe pai
+		super(message)
+		this.statusCode = statusCode
+	}
+}
