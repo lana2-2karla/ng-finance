10564886e8f47a5aef849b6aec9fcf50c53c16e5
lana2-2karla <allanakarla1702@gmail.com>
2022-11-16 21:52:34 -0300

    :construction: feat: create test get method

--- ng-finance-api/src/index.ts
+++ ng-finance-api/src/index.ts
@@ -1,4 +1,5 @@
 import express = require("express");
+import { test } from "./controllers/user.controller";
 import { AppDataSource } from "./data-source";
 
 
@@ -6,6 +7,8 @@ AppDataSource.initialize().then(async () => {
     const app = express()
 
 	app.use(express.json())
+
+	app.get('/', test)
 
 	app.listen(process.env.PORT, () => console.log(`ouvindo na porta ${process.env.PORT}`));
 
