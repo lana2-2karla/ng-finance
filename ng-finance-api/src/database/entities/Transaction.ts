69493388a46132a706a80788732c0e58380c6b5c
lana2-2karla <allanakarla1702@gmail.com>
2022-11-16 21:46:44 -0300

    :recycle: refactor: relationships in Transactions table

---
+++ ng-finance-api/src/database/entities/Transaction.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, OneToMany, JoinColumn, ManyToOne } from "typeorm"
+import Accounts from "./Account"
+
+@Entity()
+export default class Transactions {
+
+    @PrimaryGeneratedColumn("uuid")
+    id: number
+
+    @ManyToOne(() => Accounts, debited => debited.acc)
+    debitedAccountId: Accounts
+
+    @ManyToOne(() => Accounts, credited => credited.acc)
+    creditedAccountId: Accounts
+
+    @Column()
+    value: number
+
+    @CreateDateColumn({name: "created_At"})
+    createdAt: Date
+
+}
