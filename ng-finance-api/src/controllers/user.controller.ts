3ed652cb543818ca4b9c8ad3cea82dbbd3ba6307
lana2-2karla <allanakarla1702@gmail.com>
2022-11-18 18:49:07 -0300

    :recycle: refactor: constructor withdrawal

--- ng-finance-api/src/controllers/user.controller.ts
+++ ng-finance-api/src/controllers/user.controller.ts
@@ -1,17 +1,12 @@
 import { Request, Response } from "express"
-import { userRepository } from "../database/repositories/user.repository"
-import { loginservice } from "../services/login.service";
+import { Loginservice } from "../services/login.service";
 
 export class UserController {
 
-    private service: loginservice;
-
-    constructor() {
-      this.service = new loginservice();
-    }
+  private service = new Loginservice()
 
     async create(req: Request, res: Response) {
-        const newUser = await this.service.create(req.body);
+        const newUser = await this.service.create(req.body)
         res.status(200).json(newUser);
     }
 }
