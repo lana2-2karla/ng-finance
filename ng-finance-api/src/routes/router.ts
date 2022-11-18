66a0c6a7b198ad73beed3e968f33b7086b096419
lana2-2karla <allanakarla1702@gmail.com>
2022-11-18 18:50:34 -0300

    :bug: fix: fixing POST route creation method

--- ng-finance-api/src/routes/router.ts
+++ ng-finance-api/src/routes/router.ts
@@ -2,8 +2,9 @@ import { Router } from 'express'
 import { UserController } from '../controllers/user.controller'
 
 const routes = Router()
+const userController = new UserController();
 
-routes.post('/user', new UserController().create)
+routes.post('/user', (req, res) => userController.create(req, res))
 
 
 export default routes
