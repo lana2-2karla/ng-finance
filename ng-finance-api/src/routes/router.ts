a8a1e47e529f0584905b1e4bd3760ff8971e0820
lana2-2karla <allanakarla1702@gmail.com>
2022-11-19 12:26:03 -0300

    :sparkles: feat: create router /login - method POST

--- ng-finance-api/src/routes/router.ts
+++ ng-finance-api/src/routes/router.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express'
+import { LoginController } from '../controllers/login.controller';
 import { UserController } from '../controllers/register.controller'
 import registerValidate from '../middlewares/registerValidation';
 
 const routes = Router()
 const userController = new UserController();
+const loginController = new LoginController();
 
 routes.post('/user', 
 registerValidate.validateSchema,
-(req, res) => userController.create(req, res))
+(req, res) => userController.create(req, res));
+
+routes.post('/login', (req, res) => loginController.authUser(req,res));
 
 
 export default routes
