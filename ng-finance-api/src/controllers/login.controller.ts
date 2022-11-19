4f1c92afb015974f46a0ece2e44eb2a39289ba49
lana2-2karla <allanakarla1702@gmail.com>
2022-11-19 12:25:26 -0300

    :sparkles: feat: create class LoginController and method authUser

---
+++ ng-finance-api/src/controllers/login.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import { Login } from "../services/login.service";
+
+export class LoginController {
+
+    private _service = new Login();
+
+    async authUser(req: Request, res: Response): Promise<Response> {
+        const token = await this._service.authUser(req.body);
+        return res.status(200).json(token);
+    }
+}
