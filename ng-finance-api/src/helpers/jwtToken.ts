63139b306a0c8e2d44cc8e59cc83df8017c6ab52
lana2-2karla <allanakarla1702@gmail.com>
2022-11-19 17:33:17 -0300

    :bug: fix: to return id only

--- ng-finance-api/src/helpers/jwtToken.ts
+++ ng-finance-api/src/helpers/jwtToken.ts
@@ -1,6 +1,7 @@
 import { JwtPayload, sign, SignOptions, verify } from 'jsonwebtoken';
 import 'dotenv/config';
 import { ApiError } from './api-error';
+import { IPayloadId } from '../interfaces/interface';
 
 const SECRET = process.env.JWT_SECRET || '';
 
@@ -12,13 +13,13 @@ export class Token {
         algorithm: 'HS256',
       };
 
-    public generateToken(id: JwtPayload) {
+    async generateToken(id: JwtPayload) {
         return sign(id, SECRET, this._signOptions);
     }
 
-    public decodedToken(token: string) {
-        const decoded = verify(token, SECRET);
-        if(!decoded) throw new ApiError('invalid Token', 401)
-        return decoded;
+    async decodedToken(token: string) {
+        const { id } = verify(token, SECRET) as IPayloadId;
+        if(!id) throw new ApiError('invalid Token', 401)
+        return id;
     }
 }
