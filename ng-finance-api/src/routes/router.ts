55689a90b0655ada3bd26564501438d42d1360e6
lana2-2karla <allanakarla1702@gmail.com>
2022-11-19 17:36:40 -0300

    :sparkles: feat: adds authentication middleware in routes

--- ng-finance-api/src/routes/router.ts
+++ ng-finance-api/src/routes/router.ts
@@ -1,17 +1,21 @@
 import { Router } from 'express'
 import { LoginController } from '../controllers/login.controller';
 import { UserController } from '../controllers/register.controller'
+import { authTokenMiddleware } from '../middlewares/authToken';
 import registerValidate from '../middlewares/registerValidation';
 
 const routes = Router()
 const userController = new UserController();
 const loginController = new LoginController();
+const middleware = new authTokenMiddleware();
 
 routes.post('/user', 
 registerValidate.validateSchema,
 (req, res) => userController.create(req, res));
 
 routes.post('/login', (req, res) => loginController.authUser(req,res));
+
+routes.use((req, res, next) => middleware.authToken(req, res, next));
 
 routes.get('/user/:id', (req, res) => userController.getUserById(req, res));
 
