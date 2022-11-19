d123f0c373950c3d51c9cc1ecba0e5ce88347ed3
lana2-2karla <allanakarla1702@gmail.com>
2022-11-19 19:07:03 -0300

    :construction: feat: create method that captures the account by id

--- ng-finance-api/src/services/account.service.ts
+++ ng-finance-api/src/services/account.service.ts
@@ -1,7 +1,11 @@
 import Accounts from "../database/entities/Account";
 import { accountRepository } from "../database/repositories/account.repository";
+import { RegisterService } from "./register.service";
 
 export class AccountService {
+
+    private _register = new RegisterService;
+
     async create(): Promise<Accounts> {
         const newAccount = accountRepository.create({
             balance: 100
@@ -9,5 +13,9 @@ export class AccountService {
 
         await accountRepository.save(newAccount);
         return newAccount;
+    }
+
+    async getAccountById(id: string, dataId:string) {
+        const user = this._register.getUserById(id, dataId)
     }
 }
