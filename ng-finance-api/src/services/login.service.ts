03cf45aa175cfddd5bc13e65d0cd69bba3fd12ee
lana2-2karla <allanakarla1702@gmail.com>
2022-11-19 11:37:36 -0300

    :sparkles: feat: create class Login - method authUser

---
+++ ng-finance-api/src/services/login.service.ts
@@ -0,0 +1,31 @@
+import { userRepository } from "../database/repositories/user.repository";
+import { ApiError } from "../helpers/api-error";
+import { IuserCreated } from "../interfaces/interface";
+import bcrypt from 'bcrypt'
+import { Token } from "../helpers/jwtToken";
+
+export class Login {
+
+    private _token = new Token();
+
+    async authUser(userData: IuserCreated): Promise<string> {
+
+        const {username, password} = userData;
+
+        const userExists = await userRepository.findOneBy({ username });
+
+		if (!userExists) {
+			throw new ApiError('User does not exist', 400)
+		}
+
+        const passDecripted = await bcrypt.compare(password, userExists.password);
+
+        if (!passDecripted) {
+            throw new ApiError('Unauthorized', 401);
+        }
+
+        const token = this._token.generateToken(userExists.id);
+
+        return token;
+    }
+}
