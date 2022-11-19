2dee5dcd5907470952aa3ae7b84e126fbb814ab7
lana2-2karla <allanakarla1702@gmail.com>
2022-11-19 17:30:21 -0300

    :bug: fix: class instance and add await in request

--- ng-finance-api/src/middlewares/authToken.ts
+++ ng-finance-api/src/middlewares/authToken.ts
@@ -4,17 +4,17 @@ import { Token } from '../helpers/jwtToken';
 
 export class authTokenMiddleware {
 
-  private _token = new Token;
+  private _token = new Token();
 
   async authToken(req: Request, _res: Response, next: NextFunction) {
     const { authorization } = req.headers;
 
     if (!authorization) throw new ApiError('Token not found', 404);
   
-    const decoded = this._token.decodedToken(authorization);
+    const decoded = await this._token.decodedToken(authorization);
 
-    if (!decoded) throw new ApiError('Invalid token', 401)
-    
+    if (!decoded) throw new ApiError('Invalid token', 401);
+
     req.data = decoded;
 
     next();
