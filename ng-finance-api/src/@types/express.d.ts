14d082a8889b659be3f3d79e0c70ecc97fc37fdb
lana2-2karla <allanakarla1702@gmail.com>
2022-11-19 11:31:33 -0300

    :sparkles: feat: override express request

---
+++ ng-finance-api/src/@types/express.d.ts
@@ -0,0 +1,12 @@
+// sobrescrevendo o Request do express
+// req.data - senha decodificada
+
+import { JwtPayload } from "jsonwebtoken";
+
+declare global {
+	namespace Express {
+		export interface Request {
+			data: string | JwtPayload
+		}
+	}
+}
