06f88f4a7c39c7f8b250f6962002e3e4b01e8aef
lana2-2karla <allanakarla1702@gmail.com>
2022-11-19 14:46:33 -0300

    :sparkles: feat: create user return method by id

--- ng-finance-api/src/controllers/register.controller.ts
+++ ng-finance-api/src/controllers/register.controller.ts
@@ -3,10 +3,16 @@ import { RegisterService } from "../services/register.service";
 
 export class UserController {
 
-  private service = new RegisterService()
+  private _service = new RegisterService()
 
     async create(req: Request, res: Response) {
-        const newUser = await this.service.create(req.body)
+        const newUser = await this._service.create(req.body)
         res.status(200).json(newUser);
+    }
+
+    async getUserById(req: Request, res: Response): Promise<Response> {
+        const { id } = req.params;
+        const user = await this._service.getUserById(id);
+        return res.status(200).json(user);
     }
 }
