70f5cc88059e0c313214d70a6ffde129c0dc7e25
lana2-2karla <allanakarla1702@gmail.com>
2022-11-20 20:41:26 -0300

    :recycle: refactor: way to pass the parameter

--- ng-finance-api/src/controllers/account.controller.ts
+++ ng-finance-api/src/controllers/account.controller.ts
@@ -6,7 +6,7 @@ export class AccountController {
      private _service = new AccountService();
 
     async getAccount(req: Request, res: Response) {
-        const account = await this._service.getAccount(req.data);
+        const account = await this._service.getAccount(req.data.accountId);
         return res.status(200).json(account);
     }
 }
