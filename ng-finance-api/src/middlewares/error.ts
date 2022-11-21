29d56e9b5d56994cad9faf46b79c318852531815
lana2-2karla <allanakarla1702@gmail.com>
2022-11-21 19:41:10 -0300

    :ok_hand: style: lint review in error

--- ng-finance-api/src/middlewares/error.ts
+++ ng-finance-api/src/middlewares/error.ts
@@ -2,14 +2,15 @@ import { NextFunction, Request, Response } from 'express'
 import { ApiError } from '../helpers/api-error'
 
 export const errorMiddleware = (
-    //Partial - atributos de "error" se tornam opcionais
-	error: Error & Partial<ApiError>,
-	req: Request,
-	res: Response,
-	next: NextFunction
-) => {
-	const statusCode = error.statusCode ?? 500
-    // tratamento de erros inesperados
-	const message = error.statusCode ? error.message : 'Internal Server Error'
-	return res.status(statusCode).json({ message })
+  // Partial - atributos de "error" se tornam opcionais
+  error: Error & Partial<ApiError>,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Response => {
+  const statusCode = error.statusCode ?? 500
+  // tratamento de erros inesperados
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  const message = error.statusCode ? error.message : 'Internal Server Error'
+  return res.status(statusCode).json({ message })
 }
