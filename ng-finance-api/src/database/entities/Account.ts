f3fbb855c75507fc12ba3a8e40335050568ec534
lana2-2karla <allanakarla1702@gmail.com>
2022-11-21 17:20:18 -0300

    :fire: refactor: remove unused import

--- ng-finance-api/src/database/entities/Account.ts
+++ ng-finance-api/src/database/entities/Account.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm"
 import Transactions from "./Transaction"
 import Users from "./User"
 
