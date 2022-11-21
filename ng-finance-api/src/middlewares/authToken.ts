e1e24d5c61529e59459192bbf34cfffc21dd0ace
lana2-2karla <allanakarla1702@gmail.com>
2022-11-21 19:40:17 -0300

    :ok_hand: style: lint review on authToken

--- ng-finance-api/src/middlewares/authToken.ts
+++ ng-finance-api/src/middlewares/authToken.ts
@@ -1,22 +1,22 @@
-import { NextFunction, Request, Response } from 'express';
-import { ApiError } from '../helpers/api-error';
-import { Token } from '../helpers/jwtToken';
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { NextFunction, Request, Response } from 'express'
+import { ApiError } from '../helpers/api-error'
+import { Token } from '../helpers/jwtToken'
 
 export class authTokenMiddleware {
+  private readonly _token = new Token()
 
-  private _token = new Token();
+  async authToken (req: Request, _res: Response, next: NextFunction): Promise<void> {
+    const { authorization } = req.headers
 
-  async authToken(req: Request, _res: Response, next: NextFunction) {
-    const { authorization } = req.headers;
+    if (!authorization) throw new ApiError('Token not found', 404)
 
-    if (!authorization) throw new ApiError('Token not found', 404);
-  
-    const decoded = await this._token.decodedToken(authorization);
+    const decoded = await this._token.decodedToken(authorization)
 
-    if (!decoded) throw new ApiError('Invalid token', 401);
+    if (!decoded) throw new ApiError('Invalid token', 401)
 
-    req.data = decoded;
+    req.data = decoded
 
-    next();
+    next()
   }
 }
