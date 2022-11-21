9e5d1928699fb3b4515f838970d6c32ca11081ae
lana2-2karla <allanakarla1702@gmail.com>
2022-11-21 19:50:09 -0300

    :ok_hand: style: lint review on routes

--- ng-finance-api/src/routes/router.ts
+++ ng-finance-api/src/routes/router.ts
@@ -1,36 +1,38 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/promise-function-async */
 import { Router } from 'express'
-import { AccountController } from '../controllers/account.controller';
-import { LoginController } from '../controllers/login.controller';
+import { AccountController } from '../controllers/account.controller'
+import { LoginController } from '../controllers/login.controller'
 import { UserController } from '../controllers/user.controller'
-import { TransactionController } from '../controllers/transaction.controller';
-import { authTokenMiddleware } from '../middlewares/authToken';
-import registerValidate from '../middlewares/registerValidation';
+import { TransactionController } from '../controllers/transaction.controller'
+import { authTokenMiddleware } from '../middlewares/authToken'
+import registerValidate from '../middlewares/registerValidation'
 
 const routes = Router()
-const userController = new UserController();
-const loginController = new LoginController();
-const middleware = new authTokenMiddleware();
-const accountController = new AccountController();
-const transactionController = new TransactionController();
+const userController = new UserController()
+const loginController = new LoginController()
+const middleware = new authTokenMiddleware()
+const accountController = new AccountController()
+const transactionController = new TransactionController()
 
-routes.post('/user', 
-registerValidate.validateSchema,
-(req, res) => userController.create(req, res));
+routes.post('/user',
+  registerValidate.validateSchema,
+  (req, res) => userController.create(req, res))
 
-routes.post('/login', (req, res) => loginController.authUser(req,res));
+routes.post('/login', (req, res) => loginController.authUser(req,res))
 
-routes.use((req, res, next) => middleware.authToken(req, res, next));
+routes.use((req, res, next) => middleware.authToken(req, res, next))
 
-routes.get('/user', (req, res) => userController.getUser(req, res));
+routes.get('/user', (req, res) => userController.getUser(req, res))
 
-routes.get('/account/user', (req, res) => accountController.getAccount(req, res));
+routes.get('/account/user', (req, res) => accountController.getAccount(req, res))
 
-routes.post('/transaction/user', (req, res) => transactionController.create(req, res));
+routes.post('/transaction/user', (req, res) => transactionController.create(req, res))
 
-routes.get('/transaction/cashin', (req, res) => transactionController.getByTransactionCashIn(req, res));
+routes.get('/transaction/cashin', (req, res) => transactionController.getByTransactionCashIn(req, res))
 
-routes.get('/transaction/cashout', (req, res) => transactionController.getByTransactionCashOut(req, res));
+routes.get('/transaction/cashout', (req, res) => transactionController.getByTransactionCashOut(req, res))
 
-routes.get('/transactions/user', (req, res) => transactionController.getTransactions(req, res));
+routes.get('/transactions/user', (req, res) => transactionController.getTransactions(req, res))
 
 export default routes
