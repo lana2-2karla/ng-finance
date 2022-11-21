ae24c0da34c563405436618f794f04ab122e6cbd
lana2-2karla <allanakarla1702@gmail.com>
2022-11-21 20:11:02 -0300

    :ok_hand: style: lint review on data-source and index

--- ng-finance-api/src/index.ts
+++ ng-finance-api/src/index.ts
@@ -1,20 +1,19 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import 'express-async-errors'
-import express = require("express")
-import { AppDataSource } from "./data-source"
-import { errorMiddleware } from "./middlewares/error"
+import express = require('express')
+import { AppDataSource } from './data-source'
+import { errorMiddleware } from './middlewares/error'
 import routes from './routes/router'
 
-
 AppDataSource.initialize().then(async () => {
-    const app = express()
+  const app = express()
 
-	app.use(express.json())
+  app.use(express.json())
 
-	app.use(routes)
+  app.use(routes)
 
-	app.use(errorMiddleware)
+  app.use(errorMiddleware)
 
-	app.listen(process.env.PORT, () => console.log(`ouvindo na porta ${process.env.PORT}`));
-
-
+  app.listen(process.env.PORT, () => console.log(`ouvindo na porta ${process.env.PORT}`))
 })
