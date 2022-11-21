f50bbfe426a7ee06b18777d634ae282428edd83c
lana2-2karla <allanakarla1702@gmail.com>
2022-11-21 19:42:27 -0300

    :ok_hand: style: lint review in registerValidation

--- ng-finance-api/src/middlewares/registerValidation.ts
+++ ng-finance-api/src/middlewares/registerValidation.ts
@@ -1,22 +1,24 @@
-import { NextFunction, Request, Response } from 'express';
-import Joi from 'joi';
-import { ApiError } from '../helpers/api-error';
-import passwordComplexity from "joi-password-complexity";
+import { NextFunction, Request, Response } from 'express'
+import Joi from 'joi'
+import { ApiError } from '../helpers/api-error'
+import passwordComplexity from 'joi-password-complexity'
 
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 class registerValidate {
   static schema = Joi.object({
-    username : Joi.string().min(3).required(),
-    password: passwordComplexity().required(),
+    username: Joi.string().min(3).required(),
+    password: passwordComplexity().required()
   })
 
-  static validateSchema(req: Request, res: Response, next: NextFunction) {
-    const { error } = registerValidate.schema.validate(req.body);
+  static validateSchema (req: Request, res: Response, next: NextFunction): void {
+    const { error } = registerValidate.schema.validate(req.body)
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
     if (error) {
-      const { message } = error.details[0];
-      throw new ApiError(message, 400);
+      const { message } = error.details[0]
+      throw new ApiError(message, 400)
     }
-    next();
+    next()
   }
 }
 
-export default registerValidate;
+export default registerValidate
