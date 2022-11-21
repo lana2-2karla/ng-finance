7f7eba66971d99716b0803fb21a792076f98e6ca
lana2-2karla <allanakarla1702@gmail.com>
2022-11-21 19:29:32 -0300

    :ok_hand: style: lint review in api-error

--- ng-finance-api/src/helpers/api-error.ts
+++ ng-finance-api/src/helpers/api-error.ts
@@ -1,10 +1,10 @@
 export class ApiError extends Error {
-    // readonly - atributo somente para leitura
-	public readonly statusCode: number
+  // readonly - atributo somente para leitura
+  public readonly statusCode: number
 
-	constructor(message: string, statusCode: number) {
-        //super é para acessar a classe pai
-		super(message)
-		this.statusCode = statusCode
-	}
+  constructor (message: string, statusCode: number) {
+    // super é para acessar a classe pai
+    super(message)
+    this.statusCode = statusCode
+  }
 }
