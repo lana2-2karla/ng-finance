b20b3ece68ccdd42e4b10e1d1219952a32c196ba
lana2-2karla <allanakarla1702@gmail.com>
2022-11-21 19:30:48 -0300

    :ok_hand: style: lint review in jwtToken

--- ng-finance-api/src/helpers/jwtToken.ts
+++ ng-finance-api/src/helpers/jwtToken.ts
@@ -1,24 +1,22 @@
-import { JwtPayload, sign, SignOptions, verify } from 'jsonwebtoken';
-import 'dotenv/config';
-import { ApiError } from './api-error';
+import { JwtPayload, sign, SignOptions, verify } from 'jsonwebtoken'
+import 'dotenv/config'
+import { ApiError } from './api-error'
 
-const SECRET = process.env.JWT_SECRET || '';
-
+const SECRET = process.env.JWT_SECRET || ''
 
 export class Token {
-    
-    private _signOptions: SignOptions = {
-        expiresIn: '24h',
-        algorithm: 'HS256',
-      };
+  private readonly _signOptions: SignOptions = {
+    expiresIn: '24h',
+    algorithm: 'HS256'
+  }
 
-    async generateToken(payload: JwtPayload) {
-        return sign(payload, SECRET, this._signOptions);
-    }
+  async generateToken (payload: JwtPayload): Promise<string> {
+    return sign(payload, SECRET, this._signOptions)
+  }
 
-    async decodedToken(token: string) {
-        const payload = verify(token, SECRET) as JwtPayload;
-        if(!payload) throw new ApiError('invalid Token', 401)
-        return payload;
-    }
+  async decodedToken (token: string): Promise<JwtPayload> {
+    const payload = verify(token, SECRET) as JwtPayload
+    if (!payload) throw new ApiError('invalid Token', 401)
+    return payload
+  }
 }
